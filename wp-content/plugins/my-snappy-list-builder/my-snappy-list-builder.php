160d95e7e6ba81cdb7ff2ab9e8734c7e903b4b90
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-08-08 13:07:02 -0400

    custom shortcode for signup form

--- wp-content/plugins/my-snappy-list-builder/my-snappy-list-builder.php
+++ wp-content/plugins/my-snappy-list-builder/my-snappy-list-builder.php
@@ -10,4 +10,108 @@
    License URI: https://www.gnu.org/licenses/gpl-2.0.html
    Text Domain: snappy-list-builder
    */
+
+/* !0. TABLE OF CONTENTS */
+
+/*
+	
+	1. HOOKS
+        1.1 - call to register custom shortcodes
+    
+    2. SHORTCODES
+        2.1 - slb_register_shortcodes()
+        2.2 - slb_form_shortcode()
+		
+	3. FILTERS
+		
+	4. EXTERNAL SCRIPTS
+		
+	5. ACTIONS
+		
+	6. HELPERS
+		
+	7. CUSTOM POST TYPES
+	
+	8. ADMIN PAGES
+	
+	9. SETTINGS
+	
+*/
+
+
+
+
+/* !1. HOOKS */
+// 1.1
+add_action('init', 'slb_register_shortcodes');
+
+/* !2. SHORTCODES */
+// 2.1
+function slb_register_shortcodes() {
+    add_shortcode('slb_form', 'slb_form_shortcode');
+}
+// 2.2
+function slb_form_shortcode($args, $content="") {
+    $output = '
+        <div class="slb">
+            <form id="slb_form" name="slb_form" class="slb-form" method="post">
+                <p class="slb-input-container">
+                    <label>Your Name</label><br />
+                    <input type="text" name="slb_firstname" placeholder="First Name" />
+                    <input type="text" name="slb_lastname" placeholder="Last Name" />
+                </p>
+                <p class="slb-input-container">
+                    <label>Your Email</label><br />
+                    <input type="email" name="slb_email" placeholder="you@email.com" />
+                </p>
+    ';
+    if (strlen($content)):
+        $output .= '<div class="slb-content">' . wpautop($content) . '</div>';
+    endif;
+    $output .= '
+                <p class="slb-input-container">
+                    <input type="submit" name="slb_submit" value="Sign Me Up!" />
+                </p>
+            </form>
+        </div>
+    ';
+    return $output;
+}
+
+
+
+
+/* !3. FILTERS */
+
+
+
+
+/* !4. EXTERNAL SCRIPTS */
+
+
+
+
+/* !5. ACTIONS */
+
+
+
+
+/* !6. HELPERS */
+
+
+
+
+/* !7. CUSTOM POST TYPES */
+
+
+
+
+/* !8. ADMIN PAGES */
+
+
+
+
+/* !9. SETTINGS */
+
+
 ?>
