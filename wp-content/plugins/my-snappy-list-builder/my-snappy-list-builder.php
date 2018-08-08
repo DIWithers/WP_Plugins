400f23eb5358f267c12db8186162181cb640a003
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-08-08 12:29:28 -0400

    add plugin meta data

---
+++ wp-content/plugins/my-snappy-list-builder/my-snappy-list-builder.php
@@ -0,0 +1,13 @@
+<?php 
+   /* 
+   Plugin Name: My Snappy List Builder
+   Plugin URI: http://wordpressplugincourse.com/plugins/snappy-list-builder
+   Description: The ultimate email list building plugin for WordPress. Capture new subscribers. Reward subscribers with a custom download upon opt-in. Build unlimited lists. Import and export subscribers easily with .csv
+   Version: 1.0 --- Plugin Practice Following Snappy List Builder
+   Author: DW @ Protothought
+   Author URI: https://proto-thought.com
+   License: GPL2
+   License URI: https://www.gnu.org/licenses/gpl-2.0.html
+   Text Domain: snappy-list-builder
+   */
+?>
